b2ff7fa0864ab865ae4ff98f28b9ef8295d82c5b
Matt <mnabavi84@users.noreply.github.com>
2020-06-28 14:58:12 -0700

    Update 02-Cost and Common Table Expressions (CTEs).sql

--- 02-Cost and Common Table Expressions (CTEs).sql
+++ 02-Cost and Common Table Expressions (CTEs).sql
@@ -3,7 +3,8 @@ SELECT
   meals.meal_id,
   SUM (meal_cost * stocked_quantity) AS cost
 FROM meals
-JOIN stock ON meals.meal_id = stock.meal_id
+JOIN stock
+ON meals.meal_id = stock.meal_id
 GROUP BY meals.meal_id
 ORDER BY cost DESC
 -- Only the top 5 meal IDs by purchase cost
@@ -15,7 +16,8 @@ SELECT
   DATE_TRUNC('month', stocking_date)::DATE AS delivr_month,
   SUM (meal_cost * stocked_quantity) AS cost
 FROM meals
-JOIN stock ON meals.meal_id = stock.meal_id
+JOIN stock
+ON meals.meal_id = stock.meal_id
 GROUP BY delivr_month
 ORDER BY delivr_month ASC;
 
@@ -26,7 +28,8 @@ WITH monthly_cost AS (
     DATE_TRUNC('month', stocking_date)::DATE AS delivr_month,
     SUM(meal_cost * stocked_quantity) AS cost
   FROM meals
-  JOIN stock ON meals.meal_id = stock.meal_id
+  JOIN stock
+  ON meals.meal_id = stock.meal_id
   GROUP BY delivr_month)
 
 SELECT *
@@ -39,7 +42,8 @@ WITH monthly_cost AS (
     DATE_TRUNC('month', stocking_date)::DATE AS delivr_month,
     SUM(meal_cost * stocked_quantity) AS cost
   FROM meals
-  JOIN stock ON meals.meal_id = stock.meal_id
+  JOIN stock
+  ON meals.meal_id = stock.meal_id
   GROUP BY delivr_month)
 
 SELECT
@@ -47,5 +51,3 @@ SELECT
   AVG(cost)
 FROM monthly_cost
 WHERE delivr_month < '2018-09-01';
-
-
