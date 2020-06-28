d745cf738be01e8e27578f9c7528bb16318d0401
Matt <mnabavi84@users.noreply.github.com>
2020-06-28 14:58:25 -0700

    Update 01-Revenue.sql

--- 01-Revenue.sql
+++ 01-Revenue.sql
@@ -1,7 +1,8 @@
 -- Calculate revenue
 SELECT SUM (meal_price * order_quantity) AS revenue
   FROM meals
-  JOIN orders ON meals.meal_id = orders.meal_id
+  JOIN orders
+  ON meals.meal_id = orders.meal_id
 -- Keep only the records of customer ID 15
 WHERE user_id = '15';
 
@@ -10,7 +11,8 @@ SELECT DATE_TRUNC('week', order_date) :: DATE AS delivr_week,
        -- Calculate revenue
        SUM(meal_price * order_quantity) AS revenue
   FROM meals
-  JOIN orders ON meals.meal_id = orders.meal_id
+  JOIN orders
+  ON meals.meal_id = orders.meal_id
 -- Keep only the records in June 2018
 WHERE DATE_TRUNC('month', order_date) = '2018-06-01'
 GROUP BY delivr_week
